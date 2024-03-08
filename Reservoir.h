670db6dd3e654e7044c1b8b1a242b62baf46b673
EuNumShei <145541911+EuNumShei@users.noreply.github.com>
2024-03-08 22:35:09 +0000

    Create Reservoir.h

---
+++ Reservoir.h
@@ -0,0 +1,30 @@
+//
+// Created by Guilherme Teixeira on 05/03/2024.
+//
+
+#ifndef PROJECT1DATASETSMALL_RESERVOIR_H
+#define PROJECT1DATASETSMALL_RESERVOIR_H
+#include <string>
+#include "Station.h"
+using namespace std;
+
+
+class Reservoir : public Station{
+public:
+    Reservoir(string name, string municipality, int id, string code, int max_delivery, char type) : Station(id, code, type = 'R'){
+        this->name = name;
+        this->municipality = municipality;
+        this->max_delivery = max_delivery;
+    }
+
+    string get_name() {return name;}
+    string get_municipality() {return municipality;}
+    int get_max_delivery() {return max_delivery;}
+private:
+    string name;
+    string municipality;
+    int max_delivery;
+};
+
+
+#endif //PROJECT1DATASETSMALL_RESERVOIR_H
